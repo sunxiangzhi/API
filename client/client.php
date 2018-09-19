2bb592139894f7a1347ef8ef8b9a7eb3aad8d8cb
sunxiangzhi <3030058056@qq.com>
2018-09-19 15:27:02 +0800

    数据标准化输出完毕

--- client/client.php
+++ client/client.php
@@ -2,4 +2,20 @@
 
 	//客户端发起api请求
 
+	include './vendor/autoload.php';
+	
+	//请求的api地址
+	$url = 'http://localhost/api/server/server.php';
+
+
+	$curl = new Curl\Curl();
+	$curl->get($url);
+	
+	if ($curl->error) {
+	    echo $curl->error_code;
+	}else {
+	    echo $curl->response;
+	}
+	
+
  ?>
--- server/server.php
+++ server/server.php
@@ -1,5 +1,29 @@
 <?php 
 
 	//接受请求响应数据
+	$pdo = new PDO('mysql:host=localhost;dbname=zhihu;charset=utf8','root','');
+
+	$stmt = $pdo->query('select * from api');
+
+	$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
+	//定义标准化产出数据格式函数
+	function resp($data,$status,$message){
+
+		$res = [
+			'message'=>$message,//此次api请求的描述
+			'status'=>$status,//服务器响应状态码
+			'data'=>$data,
+
+		];
+
+		return json_encode($res,JSON_UNESCAPED_UNICODE);
+
+	};
+
+	echo resp($data,200,'ok');
 
  ?>
